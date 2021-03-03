86e63898f442d62e79506dfc5d8b2b2d9e09aae7
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-02 23:09:50 -0300

    chore: configure prettier

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Inicio </h1>
-      </header>
+      <h1>Inicio</h1>
     </div>
   );
 }
