6dd7b6d9a10591e145496fdf0d7998503888f996
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-06 12:22:49 -0300

    chore: style in page main

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,19 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
-  parser: "babel-eslint",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "import/prefer-default-expoirt": "off",
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-expoirt': 'off',
+    'react/state-in-constructor': 0,
+    'jsx-a11y/no-autofocus': 0,
   },
 };
--- web/src/pages/Main/index.js
+++ web/src/pages/Main/index.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Main = () => {
-  return <h1>Main</h1>;
-};
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-export default Main;
+import api from '../../services/api';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  submit = async (e) => {
+    const { newRepo, repositories } = this.state;
+
+    this.setState({ loading: true });
+    e.preventDefault();
+
+    const { data } = await api.get(`/repos/${newRepo}`);
+
+    console.log(data);
+
+    const info = {
+      name: data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, info],
+      newRepo: '',
+      loading: false,
+    });
+
+    document.querySelector('#inputSearch').focus();
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt /> Repositorios
+        </h1>
+
+        <Form onSubmit={this.submit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleChange}
+            autoFocus
+            id="inputSearch"
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
